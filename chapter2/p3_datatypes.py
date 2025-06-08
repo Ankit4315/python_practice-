03f2e13a46bb845f8a6367fbd2f5176d55aefc31
WP-SL <wpsloperations@gmail.com>
2025-06-09 00:58:43 +0530

    chapter 1, 2 completed

---
+++ chapter2/p3_datatypes.py
@@ -0,0 +1,7 @@
+# Primarily these are the following data types in Python:
+# 1. Integers // a= 1
+# 2. Floating point numbers // a = 3.44
+# 3. Strings // a = "ankit"
+# 4. Booleans // a = ture/false
+# 5. None // a = none
+
