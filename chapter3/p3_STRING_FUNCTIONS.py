9602f66dfa5ff59715846e01a5267ff07c362455
WP-SL <wpsloperations@gmail.com>
2025-06-18 13:11:42 +0530

    statr the capter 3

---
+++ chapter3/p3_STRING_FUNCTIONS.py
@@ -0,0 +1,13 @@
+# 1. len () function – This function returns the length of the strings.
+
+name = "ankit"
+print("lent of string :> ",len(name))
+
+# 2. String.endswith("it") – This function_ tells whether the variable string ends with the string "it" or not.
+# If string is "ankit", it returns true for "it" since ankit ends with it.
+
+print(name.endswith("it")) #true
+print(name.endswith("iti")) #false
+
+print(name.startswith("an")) #true
+print(name.startswith("ant")) #false
