6712c49df1134b253b8fe5e72f4840864fcba836
WP-SL <wpsloperations@gmail.com>
2025-06-23 14:04:20 +0530

    question 4 of chapter 3 completed

---
+++ chapter3/practice_set/q4.py
@@ -0,0 +1,8 @@
+# 4. Replace the double space from problem 3 with single spaces.
+
+def replace(str):
+    st = str.replace('  ', ' ')
+    print(st)
+    
+str = input('enter a string :> ')
+replace(str)
