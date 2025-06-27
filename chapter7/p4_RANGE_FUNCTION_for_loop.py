15d8ec16628fcf477a7fd33b4d211b3fe3924b70
Ankit4315 <varmaankit0996@gmail.com>
2025-06-27 20:23:33 +0530

    half of chapter 7 thory completed

---
+++ chapter7/p4_RANGE_FUNCTION_for_loop.py
@@ -0,0 +1,20 @@
+
+'''
+The range() function in python is used to generate a sequence of number. 
+
+We can also specify the start, stop and step-size as follows: 
+
+range(start, stop, step_size) # step_size is usually not used with range() 
+
+AN EXAMPLE  DEMONSTRATING  RANGE () FUNCTION.  
+ for i in range(0,7): # range(7) can also be used. 
+    print(i) # prints 0 to 6
+    
+'''
+
+for i in range(0,6):
+    print(i)
+
+
+for i in range(0,50,10):
+    print(i, end=" ")
