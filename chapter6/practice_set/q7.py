fc681b467a9bdd4a7c76a2ed314cec90daf97dc5
WP-SL <wpsloperations@gmail.com>
2025-06-27 13:47:36 +0530

    completed the practice set of the chapter 6

---
+++ chapter6/practice_set/q7.py
@@ -0,0 +1,8 @@
+# 7. Write a program to find out whether a given post is talking about “Harry” or not. 
+
+post = input("Enter a  post :> ")
+
+if("ankit".lower() in post.lower()):
+    print("about the ankit")
+else:
+    print("not about ankit")
