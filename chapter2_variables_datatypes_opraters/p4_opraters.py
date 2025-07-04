ebeff0e5114a4ecbe8735e0771a56c39a8f02624
Ankit4315 <varmaankit0996@gmail.com>
2025-07-04 13:08:20 +0530

    change chapter 2 name

---
+++ chapter2_variables_datatypes_opraters/p4_opraters.py
@@ -0,0 +1,43 @@
+# 1. Arithmetic operators: +, -, *, / etc.
+a = 7
+b= 6
+result = a + b
+print(result);
+
+# 2. Assignment operators: =, +=, -= etc.
+a = 4-2
+print(a)
+b = 6  
+print(b)
+b += 3 #Increment the velue of b by 3 and asign it to b
+print(b)
+
+# 3. Comparison operators: ==, >, >=, <, != etc.
+# --> it return allways bool value
+
+d = 5>5
+print(d)
+
+
+# 4. Logical operators: and, or, not
+
+e = True or Flase
+print(e)
+
+# truth table for or
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("Flase or True is ", False or True)
+print("Flase or Flase is ", False or False)
+
+# truth table for and
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("Flase and True is ", False and True)
+print("Flase and Flase is ", False and False)
+
+print(not(False))
+print(not(True))
+
+
+
